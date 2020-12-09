6b0b865dcfd210f71072ea6c150cf8fb14252b3e
ShadowAlpha666 <66505990+ShadowAlpha666@users.noreply.github.com>
2020-12-09 12:43:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready! You can start using me.');
+});
+
+
+client.on('message', message => {
+	if (message.includes === `a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`) {
+		if (message.author.bot) return;
+		message.channel.send('BREAD!!!!!!!!!!!!!!!!!!!');
+	} 
+});
+
+client.login(token);
