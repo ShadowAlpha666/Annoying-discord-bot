60771e8156e6154a69de03b6b7bc65777ce3752b
ShadowAlpha666 <66505990+ShadowAlpha666@users.noreply.github.com>
2020-12-09 12:48:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.once('ready', () => {
 client.on('message', message => {
 	if (message.includes === `a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`) {
 		if (message.author.bot) return;
-		message.channel.send('BREAD!!!!!!!!!!!!!!!!!!!');
+		message.channel.send('YOUR_MESSAGE_HERE');
 	} 
 });
 
